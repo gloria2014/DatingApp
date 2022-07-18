f116c4046c86c4d2569952aed76fa0f543258296
gloria2014 <gcastromerma@gmail.com>
2022-07-18 16:06:11 -0400

    init commit 1

---
+++ DatingApp_6/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using DatingApp_6.Data;
+using DatingApp_6.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace DatingApp_6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+            
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser> >GetUsers(int id)
+        {
+          return await _context.Users.FindAsync(id);
+            
+        }
+    }
+}
